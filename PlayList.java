a7568ed9e979681b2f3e9c36a1af18f341b9511b
liram kreiner <liram.kreiner@post.runi.ac.il>
2024-02-06 19:35:10 +0200

    try

--- PlayList.java
+++ PlayList.java
@@ -95,12 +95,11 @@ class PlayList {
     {
         if(this.size>=this.maxSize||i>=maxSize)
             return false;
-        for (int k=size;k<=i ;k-- ) 
+        for (int k=size;k<i ;k-- ) 
         {
             this.tracks[k]=this.tracks[k-1];
         }
         this.tracks[i]=track;
-        this.size++;
         return true;
     }
      
